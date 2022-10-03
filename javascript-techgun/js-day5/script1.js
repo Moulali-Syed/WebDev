e71e4450163fa01cac008dcbe9dad97da5dd38d0
Moulali Syed <syedmoulali1997@gmail.com>
2022-10-03 18:06:16 +0530

    day5-js

---
+++ javascript-techgun/js-day5/script1.js
@@ -0,0 +1,52 @@
+//onclick event
+// function btnClick() {
+//   alert('button was clicked');
+// }
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', btnClick);
+
+//using anonymous function
+// btn.addEventListener('click', function () {
+//   console.log('btn clicked');
+// });
+
+// btn.addEventListener('mouseover', function () {
+//   console.log('mouseover is activated');
+// });
+
+// btn.addEventListener('mouseout', function () {
+//   console.log('mouse out is activated');
+// });
+
+// let btn = document.getElementById('btn');
+
+// function click1() {
+//   console.log('click activated');
+// }
+
+// function click2() {
+//   console.log('click2 activated');
+// }
+
+// btn.addEventListener('click', click1);
+// btn.addEventListener('click', click2);
+
+// btn.removeEventListener('click', click2);
+
+//place some heavy image in the html file to see that DOMContentLoaded and then load
+// window.addEventListener('DOMContentLoaded', function () {
+//   console.log('DOM tree created');
+// });
+
+// window.addEventListener('load', function () {
+//   console.log('Fuly loaded');
+// });
+
+// let img = document.getElementById('img1');
+// img.addEventListener('load', function () {
+//   console.log('fully loaded');
+// });
+
+function fun() {
+  alert('event activated');
+}
